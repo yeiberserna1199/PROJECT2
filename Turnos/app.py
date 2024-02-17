7b1a1b1d00e95978d81f2f0ee972e304943c15c7
yeiberserna1199 <142941022+yeiberserna1199@users.noreply.github.com>
2024-02-17 08:08:41 +0000

    1st commit

---
+++ Turnos/app.py
@@ -0,0 +1,49 @@
+
+import sqlite3
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
+from functools import wraps
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+def apology():
+    return render_template("apology.html")
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+    
+
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    session.clear()
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
+    if request.method == "POST":
+        if not email or not password:
+            return apology()
+    return render_template("homepage.html")
+    
